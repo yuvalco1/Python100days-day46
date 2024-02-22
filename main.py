5b6500c40fb265e89b6c7b15cd3760f6a5643294
Yuval Cohen <yuvalco1@gmail.com>
2024-02-22 16:10:13 +0200

    added fixes to complete the project

--- main.py
+++ main.py
@@ -33,22 +33,26 @@ spotify_client_id = "f8eff7f2e6a6408692181ed953a5d578"
 spotify_client_secret = "94e7dc4032ed42dfb65a8c86237efed0"
 my_spotify_id = "21lvv76q7swazoiyutj3kgjti"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username="Yuval cohen",client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri="https://www.example.com", scope="playlist-modify-private", show_dialog=True, open_browser=True, cache_path="token.txt"))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username="Yuval cohen",client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri="https://www.example.com", scope="playlist-modify-public", show_dialog=True, open_browser=True, cache_path="token.txt"))
 songs_links = []
 user_id = sp.current_user()['id']
 
-# # search for songs in spotify
-# for i in range(len(song_names)):
-#     search_results = sp.search(q=f"{song_names[i]} artist:{artist_names[i]}", limit=1, type="track", market="US")
-#     #pprint(search_results)
-#     try:
-#         song_link = search_results['tracks']['items'][0]['external_urls']['spotify']
-#         songs_links.append(song_link)
-#     except IndexError:
-#         songs_links.append("Not found")
-# print(songs_links)
+# search for songs in spotify
+for i in range(len(song_names)):
+    search_results = sp.search(q=f"{song_names[i]} artist:{artist_names[i]}", limit=1, type="track", market="US")
+    #pprint(search_results)
+    try:
+        #song_link = search_results['tracks']['items'][0]['external_urls']['spotify'].strip("https://open.spotify.com/track/")
+        song_link = search_results['tracks']['items'][0]['uri']
+        songs_links.append(song_link)
+    except IndexError:
+        #songs_links.append("Not found")
+        print("Not found")
+print(songs_links)
 
 # create a playlist in spotify
 playlist_name = (f"{travel_date} Billboard 100")
 print(user_id)
 playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=True, collaborative=False, description="Billboard 100 Hot 100 songs for " + travel_date)
+playlist_id = playlist['id']
+sp.playlist_add_items(playlist_id, songs_links)
